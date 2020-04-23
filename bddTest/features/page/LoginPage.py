051c119b066d9a817da8d2006685c777be0b6235
LeslieLyu <37355343+Katrina-Lyu@users.noreply.github.com>
2020-04-23 12:40:07 +0800

    Update LoginPage.py

--- bddTest/features/page/LoginPage.py
+++ bddTest/features/page/LoginPage.py
@@ -2,25 +2,25 @@
 class LoginPage():
 
     # First welcome page
-    welcomeImageId_id = "sg.com.trustedsource.boardVision:id/latest_img"
-    continueBtn1_id = "sg.com.trustedsource.boardVision:id/continue_to"
+    welcomeImageId_id = "sg.com"
+    continueBtn1_id = "sg.com"
     # Second welcome page
-    welcomeImageId2_id = "sg.com.trustedsource.boardVision:id/decision"
-    continueBtn2_id = "sg.com.trustedsource.boardVision:id/continue_to"
+    welcomeImageId2_id = "sg.com"
+    continueBtn2_id = "sg.com"
     # Select login page
-    emailAddBtn_id = "sg.com.trustedsource.boardVision:id/emailLoginLayout"
+    emailAddBtn_id = "sg.com"
     # Key in username and PW page -xpath element
-    userNameBox_xp = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[1]/android.widget.FrameLayout[1]/android.webkit.WebView/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.view.View/android.view.View/android.view.View[1]/android.view.View/android.widget.EditText"
-    PWBox_xp = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[1]/android.widget.FrameLayout[1]/android.webkit.WebView/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.view.View/android.view.View/android.view.View[2]/android.view.View[1]/android.view.View/android.widget.EditText"
-    loginBtn_xp = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[1]/android.widget.FrameLayout[1]/android.webkit.WebView/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.Button"
-    wrongPWMsg_xp = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[1]/android.widget.FrameLayout[1]/android.webkit.WebView/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.view.View"
-    PWbox2_xp = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[1]/android.widget.FrameLayout[1]/android.webkit.WebView/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[3]/android.view.View/android.view.View/android.view.View[2]/android.view.View[1]/android.view.View/android.widget.EditText"
+    userNameBox_xp = "EditText"
+    PWBox_xp = "EditText"
+    loginBtn_xp = "Button"
+    wrongPWMsg_xp = "View"
+    PWbox2_xp = "EditText"
     # Reset password page
-    forgetPWBtn_id = "new UiSelector().text(\"Don't remember your password?\")"
-    emailAddBox_xp = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[1]/android.widget.FrameLayout[2]/android.webkit.WebView/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.view.View/android.view.View/android.view.View[2]/android.view.View/android.widget.EditText"
+    forgetPWBtn_id = "new"
+    emailAddBox_xp = "EditText"
     # Id related to PIN
-    createPINId_id = "sg.com.trustedsource.boardVision:id/createpin"
-    eraseBtn_id = "sg.com.trustedsource.boardVision:id/erase"
-    numb10_id = "sg.com.trustedsource.boardVision:id/numb10"
-    confirmBtn_id = "sg.com.trustedsource.boardVision:id/resendotp"
-    forgetPINBtn_id = "sg.com.trustedsource.boardVision:id/forgot"
+    createPINId_id = "sg.com"
+    eraseBtn_id = "sg.com"
+    numb10_id = "sg.com"
+    confirmBtn_id = "sg.com"
+    forgetPINBtn_id = "sg.com"
