8c61400bdafd19893b26a6cafec409915977f9d5
LeslieLyu <37355343+Katrina-Lyu@users.noreply.github.com>
2020-04-23 12:38:03 +0800

    Update ItemPage.py

--- bddTest/features/page/ItemPage.py
+++ bddTest/features/page/ItemPage.py
@@ -1,18 +1,18 @@
 class ItemPage():
     # here are elements' ids or x_paths in item page
-    itemPageTitle_id = "sg.com.trustedsource.boardVision:id/title"
-    topItem_xp = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/androidx.viewpager.widget.ViewPager/android.widget.RelativeLayout/android.view.ViewGroup/android.widget.ScrollView/android.widget.RelativeLayout/android.widget.RelativeLayout[2]/androidx.recyclerview.widget.RecyclerView/android.widget.RelativeLayout[1]/android.widget.RelativeLayout/android.widget.TextView[3]"
-    topItemText_xp = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/androidx.viewpager.widget.ViewPager/android.widget.RelativeLayout/android.view.ViewGroup/android.widget.ScrollView/android.widget.RelativeLayout/android.widget.RelativeLayout/androidx.recyclerview.widget.RecyclerView/android.widget.RelativeLayout[1]/android.widget.RelativeLayout/android.widget.TextView[3]"
+    itemPageTitle_id = "sg.com"
+    topItem_xp = "TextView[3]"
+    topItemText_xp = "TextView[3]"
 
-    filterBtn_id = "sg.com.trustedsource.boardVision:id/all_categories_v2"
-    resolutionSwitch_id = "sg.com.trustedsource.boardVision:id/resolution_switch"
-    scheduleSwitch_id = "sg.com.trustedsource.boardVision:id/scheduling_switch"
-    meetingSwitch_id = "sg.com.trustedsource.boardVision:id/meeting_switch"
+    filterBtn_id = "sg.com"
+    resolutionSwitch_id = "sg.com"
+    scheduleSwitch_id = "sg.com"
+    meetingSwitch_id = "sg.com"
     showResultsBtn_id = "sg.com.trustedsource.boardVision:id/decline"
 
-    searchBarBtn_id = "sg.com.trustedsource.boardVision:id/new_search_v2"
-    searchBtn_id = "sg.com.trustedsource.boardVision:id/searchIcon"
-    searchTextBox_id = "sg.com.trustedsource.boardVision:id/search_text"
-    searchCloseBtn_id = "sg.com.trustedsource.boardVision:id/closeicon"
+    searchBarBtn_id = "sg.com"
+    searchBtn_id = "sg.com"
+    searchTextBox_id = "sg.com"
+    searchCloseBtn_id = "sg.com"
 
-    itemTypeText_rssid = "sg.com.trustedsource.boardVision:id/type_v2"
+    itemTypeText_rssid = "sg.com"
