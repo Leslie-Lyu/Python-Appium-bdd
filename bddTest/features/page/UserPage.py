791edf6a4114c9977bea85b9ca63da5a73006064
LeslieLyu <37355343+Katrina-Lyu@users.noreply.github.com>
2020-04-23 12:41:19 +0800

    Update UserPage.py

--- bddTest/features/page/UserPage.py
+++ bddTest/features/page/UserPage.py
@@ -1,28 +1,28 @@
 class UserPage():
     # here are ids or x_paths for elements in user page
-    userPic_id = "sg.com.trustedsource.boardVision:id/userpic_v2"
+    userPic_id = "sg.com"
 
-    myProfilePage_id = "sg.com.trustedsource.boardVision:id/myprofile_container"
+    myProfilePage_id = "sg.com"
     # My Profile page components
-    myProfilePageTitle_id = "sg.com.trustedsource.boardVision:id/toolbar_title"
-    emailAddress_id = "sg.com.trustedsource.boardVision:id/user_email_v2"
-    myProfileBackBtn_id = "sg.com.trustedsource.boardVision:id/back"
+    myProfilePageTitle_id = "sg.com"
+    emailAddress_id = "sg.com"
+    myProfileBackBtn_id = "sg.com"
 
     # Setting page and internal components
-    settingPage_id = "sg.com.trustedsource.boardVision:id/settings_container"
-    settingTitle_xp = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.RelativeLayout/android.widget.RelativeLayout/android.widget.RelativeLayout[1]/android.widget.TextView"
-    settingPageBackBtn_id = "sg.com.trustedsource.boardVision:id/back1"
-    changePIN_id = "sg.com.trustedsource.boardVision:id/createPin"
-    num0Btn_id = "sg.com.trustedsource.boardVision:id/numb10"
-    num1Btn_id = "sg.com.trustedsource.boardVision:id/numb1"
-    confirmPINChange_id = "sg.com.trustedsource.boardVision:id/resendotp"
-    eraseBtn_id = "sg.com.trustedsource.boardVision:id/erase"
-    errorMark_id = "sg.com.trustedsource.boardVision:id/entered_wrong"
-    erasePIN_id = "sg.com.trustedsource.boardVision:id/erase"
+    settingPage_id = "sg.com"
+    settingTitle_xp = "TextView"
+    settingPageBackBtn_id = "sg.com"
+    changePIN_id = "sg.com"
+    num0Btn_id = "sg.com"
+    num1Btn_id = "sg.com"
+    confirmPINChange_id = "sg.com"
+    eraseBtn_id = "sg.com"
+    errorMark_id = "sg.com"
+    erasePIN_id = "sg.com"
     
 
-    signOutBtn_id = "sg.com.trustedsource.boardVision:id/sign_out_container"
-    signOutPop_id = "sg.com.trustedsource.boardVision:id/closelayout"
-    signOutDecline_id = "sg.com.trustedsource.boardVision:id/decline"
-    signOut_xp = "/hierarchy/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.RelativeLayout/android.widget.RelativeLayout[1]/android.widget.TextView"
-    loginPage_id = "sg.com.trustedsource.boardVision:id/loginpage"
+    signOutBtn_id = "sg.com"
+    signOutPop_id = "sg.com"
+    signOutDecline_id = "sg.com"
+    signOut_xp = "TextView"
+    loginPage_id = "sg.com"
